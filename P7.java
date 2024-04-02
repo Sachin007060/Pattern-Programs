47f2d1e9b6b8a0739f4b5f3d392b818798868e8d
Sachin007060 <sachinbaskandi00@gmail.com>
2024-04-02 22:25:10 +0530

    Add pattern Program 6 and 7 in java and commit it and push it into the remote repo

---
+++ P7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class P7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(i*j+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
