ee2cf0e8179ebaa94dedc45dbb556da2d336de1a
Sachin007060 <sachinbaskandi00@gmail.com>
2024-04-07 11:19:52 +0530

    try basic pattern to revice

---
+++ Practice1.java
@@ -0,0 +1,21 @@
+// Learning patterns program so try making these pattern program for starting..
+
+// 1st :-
+
+// public class Practice1 {
+//     public static void main(String[] args) {
+//         for(int i=1; i<=5; i++){
+//             System.out.print("*"+" ");
+//         }
+//     }
+// }
+
+// 2nd :-
+
+public class Practice1 {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            System.out.println("*");
+        }
+    }
+}
---
+++ Practice2.java
@@ -0,0 +1,27 @@
+// Outer loop will decide how many rows will be printed example if (i<=5) then in this case 5 rows will be printed..
+
+// public class Practice2 {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 5; i++) {
+//             for (int j = 1; j <= 5; j++) {
+//                 System.out.print("Row:"+i+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Outer loop will decide how many rows will be printed example if (i<=7) then in this case 7 rows will be printed..
+
+public class Practice2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) {
+                System.out.print("Row:"+i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
