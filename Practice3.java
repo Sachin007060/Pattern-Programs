681e5be942401c7d4faa25ef3c79ae5e0d0e079f
Sachin007060 <sachinbaskandi00@gmail.com>
2024-04-09 14:09:46 +0530

    add a basic pattern program to built stong concept

---
+++ Practice3.java
@@ -0,0 +1,27 @@
+// Inner loop decides how many columns will be printed example :- if(j<=4) then 4 columns willl get printed.
+
+// public class Practice3 {
+//     public static void main(String[] args) {
+//         for (int i = 1; i <= 4; i++) {
+//             for (int j = 1; j <= 4; j++) {
+//                 System.out.print("Column:"+j+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Inner loop decides how many columns will be printed example :- if(j<=7) then 7 columns willl get printed.
+
+public class Practice3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) {
+                System.out.print("Column:"+j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
