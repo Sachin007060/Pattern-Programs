6cce981af4b4cedcf64a4e1cb17aad1dc40a08f2
Sachin007060 <sachinbaskandi20@gmail.com>
2024-04-10 12:10:08 +0530

    add a basic pattern program to built strong concept in java

---
+++ Practice4.java
@@ -0,0 +1,11 @@
+public class Practice4 {
+    public static void main(String[] args) {
+        int n=5; 
+        for(int i=1; i<=n+3; i++){
+            for(int j=1; j<=n+2; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
