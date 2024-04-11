3a2f484b06d553bf82f0dc3775b90f62393299f4
Sachin007060 <sachinbaskandi20@gmail.com>
2024-04-11 19:46:38 +0530

    pattern program push into the remote repo

---
+++ Practice5.java
@@ -0,0 +1,11 @@
+public class Practice5 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<= 2*n-4; i++){
+            for(int j=1; j<=n+2; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
