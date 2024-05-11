92a2e04f36dbc3523be39d8156933e684e3109e0
Sachin007060 <sachinbaskandi20@gmail.com>
2024-05-11 22:01:37 +0530

    modification commit

--- DemoPatterns.java
+++ DemoPatterns.java
@@ -8,15 +8,18 @@ import java.util.Scanner;
 
 public class DemoPatterns {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter any number: ");
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                System.out.print("*" + " ");
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter any number: ");
+        // int n = sc.nextInt();
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 2; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= 4; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
-        sc.close();
+        // sc.close();
     }
 }
